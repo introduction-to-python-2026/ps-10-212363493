9741af1c043aec87255f040bb6b0e4188b197882
212363493 <sandra_sh08@outlook.com>
2026-01-01 14:36:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from image_utils import load_image, edge_detection
-from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image
+from skimage.filters import median
+from skimage.morphology import disk
+from scipy.signal import convolve2d
 
-my_img = load_image("/content/my_picture.jpg")
-edges = edge_detection(my_img, median_size=3, gamma=0.5)
+def load_image(image_path):
+    image = Image.open(image_path).convert('RGB')
+    return np.array(image)
 
-# המרה ל-uint8 0-255
-edges_uint8 = (edges.astype(np.uint8) * 255)
-
-plt.figure(figsize=(12,6))
-
-plt.subplot(1,2,1)
-plt.imshow(my_img)
-plt.axis('off')
-plt.title("Original Image")
-
-plt.subplot(1,2,2)
-plt.imshow(edges_uint8, cmap='gray')
-plt.axis('off')
-plt.title("Edge Detected Image")
-
-plt.show()
-
-edge_image_pil = Image.fromarray(edges_uint8)
-edge_image_pil.save("my_edges.png")
+def edge_detection(my_img_array, median_size=3, gamma=0.5):
+    gray_image = np.mean(my_img_array, axis=2).astype(np.float32)
+    clean_image = median(gray_image, footprint=disk(median_size))
+    
+    kernelY = np.array([[1,0,-1],[2,0,-2],[1,0,-1]])
+    kernelX = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+    
+    edgeY = convolve2d(clean_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(clean_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+    
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    edgeMAG = edgeMAG / edgeMAG.max() * 255  # מגדילים ל-0-255
+    edgeMAG = edgeMAG ** gamma
+    
+    return edgeMAG  # לא עושים thresholding, מחזירים מספרי
