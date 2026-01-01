046abaed68ff383362fd15d70d2fba700d5b8397
212363493 <sandra_sh08@outlook.com>
2026-01-01 13:54:48 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,27 @@
+from image_utils import load_image, edge_detection
 from PIL import Image
+from skimage.filters import median
+from skimage.morphology import ball
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import convolve2d
 
-def load_image(path):
-    pass # Replace the `pass` with your code
+def load_image(image_path):
+    image = Image.open(image_path)
+    image_array = np.array(image)
+    return image_array
 
-def edge_detection(image):
-    pass # Replace the `pass` with your code
+my_img = load_image("/content/my_picture.jpg")
+plt.imshow(my_img)
+print(my_img.dtype)     
+print(my_img.shape)
+
+
+def edge_detection(my_img_array):
+    gray_image = np.mean(my_img_array, axis=2)
+    plt.imshow(gray_image, cmap='gray')
+    plt.show()
+    print(gray_image.shape)
+    print(gray_image.dtype)
+
+edge_detection(my_img)
