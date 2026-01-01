bfdb45bac175eceef0f32f59d1b4a7aa17676aa5
212363493 <sandra_sh08@outlook.com>
2026-01-01 14:31:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 my_img = load_image("/content/my_picture.jpg")
-
 edges = edge_detection(my_img, median_size=3, gamma=0.5)
 
 plt.figure(figsize=(12,6))
@@ -15,7 +14,7 @@ plt.axis('off')
 plt.title("Original Image")
 
 plt.subplot(1,2,2)
-plt.imshow(edges, cmap='gray')
+plt.imshow(edges.astype(np.uint8) * 255, cmap='gray')
 plt.axis('off')
 plt.title("Edge Detected Image")
 
@@ -24,4 +23,3 @@ plt.show()
 edge_image_to_save = (edges.astype(np.uint8) * 255)
 edge_image_pil = Image.fromarray(edge_image_to_save)
 edge_image_pil.save("my_edges.png")
-
