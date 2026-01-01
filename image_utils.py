ce72bcf7ce662bc143b06a4e23116dfbe6d0a4dc
212363493 <sandra_sh08@outlook.com>
2026-01-01 14:34:23 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -5,12 +5,12 @@ from skimage.morphology import disk
 from scipy.signal import convolve2d
 
 def load_image(image_path):
-    image = Image.open(image_path)
+    image = Image.open(image_path).convert('RGB')  # מוודא 3 ערוצים בלבד
     return np.array(image)
 
 def edge_detection(my_img_array, median_size=3, gamma=0.5):
-    gray_image = np.mean(my_img_array, axis=2).astype(np.float64)
-    clean_image = median(gray_image, disk(median_size))
+    gray_image = np.mean(my_img_array, axis=2).astype(np.float32) / 255.0  # ערכים 0-1
+    clean_image = median(gray_image, footprint=disk(median_size))
     
     kernelY = np.array([[1,0,-1],[2,0,-2],[1,0,-1]])
     kernelX = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
