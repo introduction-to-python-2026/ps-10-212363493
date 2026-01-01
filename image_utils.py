f241aa9ce928f4dfd61a00f3457d4bbccc94150a
212363493 <sandra_sh08@outlook.com>
2026-01-01 14:37:58 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,28 +1,22 @@
 import numpy as np
 from PIL import Image
-from skimage.filters import median, threshold_otsu
-from skimage.morphology import disk
 from scipy.signal import convolve2d
 
 def load_image(image_path):
-    image = Image.open(image_path).convert('RGB')  # מוודא 3 ערוצים בלבד
+    image = Image.open(image_path).convert('RGB')
     return np.array(image)
 
-def edge_detection(my_img_array, median_size=3, gamma=0.5):
-    gray_image = np.mean(my_img_array, axis=2).astype(np.float32) / 255.0  # ערכים 0-1
-    clean_image = median(gray_image, footprint=disk(median_size))
+def edge_detection(my_img_array, gamma=0.5):
+    gray_image = np.mean(my_img_array, axis=2).astype(np.float32)
     
     kernelY = np.array([[1,0,-1],[2,0,-2],[1,0,-1]])
     kernelX = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
     
-    edgeY = convolve2d(clean_image, kernelY, mode='same', boundary='fill', fillvalue=0)
-    edgeX = convolve2d(clean_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+    edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='fill', fillvalue=0)
     
     edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
-    edgeMAG = edgeMAG / edgeMAG.max()
+    edgeMAG = edgeMAG / edgeMAG.max() * 255  # 0-255
     edgeMAG = edgeMAG ** gamma
     
-    thresh = threshold_otsu(edgeMAG)
-    edge_binary = edgeMAG > thresh
-    
-    return edge_binary
+    return edgeMAG  # לא עושים thresholding
